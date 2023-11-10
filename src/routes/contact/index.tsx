b34dbd962969177ecbf82b264f355bada9cf6ecc
Makhazat Suleiman <suleimanmakhazat@gmail.com>
2023-11-10 15:25:46 +0100

    new updates

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -5,49 +5,159 @@ export default component$(()  => {
      useStylesScoped$(ContactStyles)
 
      const formVisible = useSignal(false)
-     const formState = useStore({name: "", message: "" })
+     const formState = useStore({name: "", date: "", email: "", ticketno: "", airplaneno: "", 
+           arrivetime: "", departtime: "", delay: "", reason: "", message: "" })
 
       return(
 
         <article>
-            <h2>Contact</h2>
+            <h2>Flight Details</h2>
             <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quaerat in perferendis molestiae, odit aperiam voluptate veniam 
-                doloremque provident nulla optio, laborum quas, tempore est doloribus. Rerum aperiam cumque quod!
+                Please click the button below to register your flight details.
             </p>
 
-            <button onClick$={() => formVisible.value = true}>Contact Me</button>
+            <button onClick$={() => formVisible.value = true}>Flight Details</button>
 
-            {formVisible.value && (
-        <form preventdefault:submit onSubmit$={() => {
-          console.log(formState.name, formState.message)
+          {formVisible.value && (
+          <form preventdefault:submit onSubmit$={() => {
+          console.log(formState.name, formState.email, formState.date, formState.ticketno, formState.departtime,
+                     formState.arrivetime, formState.airplaneno, formState.delay, formState.reason, formState.message)
           formState.name = ""
+          formState.email = ""
+          formState.ticketno = ""
+          formState.airplaneno = ""
+          formState.date = ""
+          formState.departtime = ""
+          formState.arrivetime = ""
+          formState.reason = ""
           formState.message = ""
+          formState.delay = ""
         }}>
-          <label for="">
+          <div class= "Details">
+            <label for="">
             <span>Your Name</span>
             <input 
                  value= {formState.name}
                  type= "text"
-                  onInput$={(e) => formState.name = (e.target as HTMLInputElement).value} 
+                 onInput$={(e) => formState.name = (e.target as HTMLInputElement).value}
+                 required 
             />
+             </label>
+
+            <label for="">
+            <span>Date</span>
+            <input 
+                 value= {formState.date}
+                 type= "date"
+                 onInput$={(e) => formState.date = (e.target as HTMLInputElement).value}
+                 required 
+            />
+            </label>
             
+            
+            <label for="">
+              <span>Email</span>
+              <input 
+              value= {formState.email}
+              type="email" 
+              onInput$={(e) => formState.email = (e.target as HTMLInputElement).value}
+              required
+              />
+            </label>
+
+            <label for="">
+            <span>Airplane No</span>
+            <input 
+                 value= {formState.airplaneno}
+                 type= "text"
+                 onInput$={(e) => formState.ticketno = (e.target as HTMLInputElement).value}
+                 required 
+            />
+            </label>
+
+            <label for="">
+            <span>Ticket No</span>
+            <input 
+                 value= {formState.ticketno}
+                 type= "text"
+                 onInput$={(e) => formState.ticketno = (e.target as HTMLInputElement).value}
+                 required 
+            />
+            </label>
+
+            <label for="">
+            <span>Departure Time</span>
+            <input 
+                 value= {formState.departtime}
+                 type= "time"
+                 onInput$={(e) => formState.departtime = (e.target as HTMLInputElement).value}
+                 required 
+            />
+            </label>
+
+            <label for="">
+            <span>Arrival Time</span>
+            <input 
+                 value= {formState.arrivetime}
+                 type= "time"
+                 onInput$={(e) => formState.arrivetime = (e.target as HTMLInputElement).value}
+                 required 
+            />
+            </label>
+             
+            <label for="">
+            <span>Delay Duration</span>
+            <input 
+                 value= {formState.delay}
+                 type= "text"
+                 onInput$={(e) => formState.delay = (e.target as HTMLInputElement).value}
+                 required 
+            />
+            </label>
+             
+            
+            <label for="">
+            <span>Reason For Delay</span>
+            <textarea 
+            value={formState.reason}
+            onInput$={(e) => formState.reason = (e.target as HTMLInputElement).value} 
+            required
+            ></textarea>
           </label>
 
           <label for="">
-            <span>Your Message</span>
+            <span>Remarks</span>
             <textarea 
             value={formState.message}
             onInput$={(e) => formState.message = (e.target as HTMLInputElement).value} 
+            required
             ></textarea>
           </label>
-          <button>Send</button>
+        </div>
+          
+
+          <button>Submit</button>
 
           <p>
             {formState.name}
           </p>
           <p>
+            {formState.airplaneno}
+          </p>
+          <p>
             {formState.message}
+          </p>
+          <p>
+            {formState.ticketno}
+          </p>
+          <p>
+            {formState.email}
+          </p>
+          <p>
+            {formState.date}
+          </p>
+          <p>
+            {formState.reason}
           </p>
         </form>
          )}
